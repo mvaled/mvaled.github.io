72338d23b1b27ce944581f4a3fe8de04120f116a
⟨m∀⟩ <mva.led@gmail.com>
2025-07-26 19:46:31 +0200

    Add finalized color-scheme selector.

---
+++ theme/js/mva.js
@@ -0,0 +1,50 @@
+const validUserSelectedSchemes = new Set(["auto", "light", "dark"]);
+
+function onColorSchemeChange() {
+    let userSelectedScheme = localStorage.getItem('userColorScheme');
+    if (!validUserSelectedSchemes.has(userSelectedScheme)) {
+        userSelectedScheme="auto"
+    }
+    if (userSelectedScheme != "auto") {
+        document.documentElement.setAttribute('data-color-scheme', userSelectedScheme)
+    } else {
+        let preferredScheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
+        document.documentElement.setAttribute('data-color-scheme', preferredScheme)
+    }
+    document.documentElement.setAttribute('data-user-color-scheme', userSelectedScheme)
+}
+
+function setUserColorScheme(scheme) {
+    if (validUserSelectedSchemes.has(scheme)) {
+        if (scheme != "auto") {
+            localStorage.setItem('userColorScheme', scheme);
+        } else {
+            localStorage.removeItem('userColorScheme');
+        }
+        onColorSchemeChange();
+    }
+}
+
+function replaceSectionHeader() {
+    document.querySelectorAll('div.section[id]').forEach(function (section) {
+        var header = section.querySelector('h1, h2, h3, h4, h5, h6');
+        if (header) {
+            header.classList.add('section-header');
+            var id = section.id;
+            var link = document.createElement('a');
+            link.classList.add('section-link');
+            link.href = `#${id}`;
+            link.textContent = header.textContent;
+            header.textContent = '';
+            header.appendChild(link);
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const handleChange = _ => onColorSchemeChange();
+    colorSchemeQuery.addEventListener('change', handleChange);
+
+    replaceSectionHeader();
+});
